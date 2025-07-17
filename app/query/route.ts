13ca36f39b02dfc99960b2d4499fdd8d86212d3c
cyvac1 <cyvac1@naver.com>
2025-07-17 15:01:45 +0900

    Chapter 6

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,6 @@
 // import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
 
 // async function listInvoices() {
 // 	const data = await sql`
@@ -15,8 +15,7 @@
 
 export async function GET() {
   return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    message: "Uncomment this file and remove this line. You can delete this file when you are finished.",
   });
   // try {
   // 	return Response.json(await listInvoices());
